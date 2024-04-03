2f2a008d542da046dde14cb8cf5edbeb808180d4
VALENTIN ROUSSEAU <59ichiigo@gmail.com>
2024-04-03 16:38:37 +0200

    remove yew , add leptos

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,14 @@
-use yew::prelude::*;
-use yew_router::prelude::*;
-
-mod video;
-
-use video::videos::Videos;
-
-#[derive(Clone, Routable, PartialEq)]
-enum Route {
-    #[at("/")]
-    Home,
-    #[at("/video")]
-    Video,
-    #[at("/*path")]
-    Misc { path: String },
-}
-
-fn switch(route: Route) -> Html {
-    match route {
-        Route::Home => html! { <h1>{ "Home" }</h1> },
-        Route::Video => html! { <Videos /> },
-        Route::Misc { path } => html! {
-                <>
-                    <p>{format!("Matched some other path: {}", path)}</p>
-                    <MyComponent />
-                </>
-            },
-    }
-}
-
-
-#[function_component(MyComponent)]
-pub fn my_component() -> Html {
-    let navigator = use_navigator().unwrap();
-    let onclick = Callback::from(move |_| navigator.push(&Route::Home));
-
-    html! {
-        <>
-            <button {onclick}>{"Click to go home"}</button>
-        </>
-    }
-}
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <BrowserRouter>
-            <Switch<Route> render={switch} /> // <- must be child of <BrowserRouter>
-        </BrowserRouter>
-    }
-    
-}
+use leptos::*;
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    console_log::init_with_level(log::Level::Trace).expect("error initializing logger");
+    console_error_panic_hook::set_once();
+
+    log::trace!("trace");
+    log::debug!("debug");
+    log::info!("info");
+    log::warn!("warn");
+    log::error!("error");
+
+    mount_to_body(|| view! { <p>"Hello, world!"</p> })
 }
